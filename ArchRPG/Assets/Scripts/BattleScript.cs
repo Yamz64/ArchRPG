4d2a759506873afd97e09ad4dd67bf3ea939e565
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2021-01-11 16:19:08 -0500

    Worked on battle scene
    Modified player prefab (got rid of buttons)
    Added ability to spawn in 4 party members

--- ArchRPG/Assets/Scripts/BattleScript.cs
+++ ArchRPG/Assets/Scripts/BattleScript.cs
@@ -42,12 +42,21 @@ public class BattleScript : MonoBehaviour
     public Text dialogue;
 
     public GameObject playerPrefab;
+    public GameObject member1Prefab;
+    public GameObject member2Prefab;
+    public GameObject member3Prefab;
     public GameObject enemyPrefab;
 
     public Transform playerStation;
+    public Transform member1Station;
+    public Transform member2Station;
+    public Transform member3Station;
     public Transform enemyStation;
 
     unit playerUnit;
+    unit member1Unit;
+    unit member2Unit;
+    unit member3Unit;
     unit enemyUnit;
 
     void Start()
@@ -63,6 +72,24 @@ public class BattleScript : MonoBehaviour
 
         GameObject enemyGo = Instantiate(enemyPrefab, enemyStation);
         enemyUnit = enemyGo.GetComponent<unit>();
+
+        if (member1Prefab && member1Station)
+        {
+            GameObject member1Go = Instantiate(member1Prefab, member1Station);
+            member1Unit = member1Go.GetComponent<unit>();
+        }
+
+        if (member2Prefab && member2Station)
+        {
+            GameObject member2Go = Instantiate(member2Prefab, member2Station);
+            member2Unit = member2Go.GetComponent<unit>();
+        }
+
+        if (member3Prefab && member3Station)
+        {
+            GameObject member3Go = Instantiate(member3Prefab, member3Station);
+            member3Unit = member3Go.GetComponent<unit>();
+        }
 
         dialogue.text = "The " + enemyUnit.unitName + " appears.";
     }
