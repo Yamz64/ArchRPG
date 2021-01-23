c1b0babf1ea81879eca9133049ee0367204741c6
James Lillicrap <jamesplillicrap@gmail.com>
2021-01-23 14:43:23 -0500

    +Added a base update stats function
    This function allows a character to hardcoded add stats based off of a level up or loaded savedata

--- ArchRPG/Assets/Scripts/CharacterStats.cs
+++ ArchRPG/Assets/Scripts/CharacterStats.cs
@@ -65,36 +65,311 @@ public class CharacterStats
     public virtual void SetWeapon(Weapon w)
     {
         weapon = w;
+        if (weapon != null) ;
         w.SetWeapon(this);
     }
     public virtual void SetArmor(Armor a)
     {
         armor = a;
+        if(armor != null)
         a.SetArmor(this);
     }
     public virtual void SetTrinket(Trinket t)
     {
         trinket = t;
+        if(trinket != null)
         t.SetTrinket(this);
     }
     public virtual void RemoveWeapon()
     {
-        weapon.RemoveWeapon();
-        weapon = null;
+        if (weapon != null)
+        {
+            weapon.RemoveWeapon();
+            weapon = null;
+        }
     }
     public virtual void RemoveArmor()
     {
-        armor.RemoveArmor();
-        armor = null;
+        if (armor != null)
+        {
+            armor.RemoveArmor();
+            armor = null;
+        }
     }
     public virtual void RemoveTrinket()
     {
-        trinket.RemoveTrinket();
-        trinket = null;
+        if (trinket != null)
+        {
+            trinket.RemoveTrinket();
+            trinket = null;
+        }
     }
     public virtual void AddAbility(Ability a)
     {
         abilities.Add(a);
+    }
+
+    public virtual void UpdateStats()
+    {
+        //remove base weapon armor and trinket
+        Weapon temp_weapon = weapon;
+        Armor temp_armor = armor;
+        Trinket temp_trinket = trinket;
+
+        RemoveWeapon();
+        RemoveArmor();
+        RemoveTrinket();
+
+        switch (LVL)
+        {
+            case 1:
+                //apply the base stats per level
+                HP_max = 26;
+                SP_max = 17;
+                ATK = 6;
+                POW = 6;
+                DEF = 6;
+                WIL = 6;
+                RES = 6;
+                SPD = 6;
+                LCK = 6;
+                break;
+            case 2:
+                //apply the base stats per level
+                HP_max = 45;
+                SP_max = 22;
+                ATK = 11;
+                POW = 11;
+                DEF = 11;
+                WIL = 11;
+                RES = 11;
+                SPD = 11;
+                LCK = 11;
+                break;
+            case 3:
+                //apply the base stats per level
+                HP_max = 64;
+                SP_max = 27;
+                ATK = 16;
+                POW = 16;
+                DEF = 16;
+                WIL = 16;
+                RES = 16;
+                SPD = 16;
+                LCK = 16;
+                break;
+            case 4:
+                //apply the base stats per level
+                HP_max = 83;
+                SP_max = 32;
+                ATK = 21;
+                POW = 21;
+                DEF = 21;
+                WIL = 21;
+                RES = 21;
+                SPD = 21;
+                LCK = 21;
+                break;
+            case 5:
+                //apply the base stats per level
+                HP_max = 102;
+                SP_max = 37;
+                ATK = 26;
+                POW = 26;
+                DEF = 26;
+                WIL = 26;
+                RES = 26;
+                SPD = 26;
+                LCK = 26;
+                break;
+            case 6:
+                //apply the base stats per level
+                HP_max = 121;
+                SP_max = 42;
+                ATK = 31;
+                POW = 31;
+                DEF = 31;
+                WIL = 31;
+                RES = 31;
+                SPD = 31;
+                LCK = 31;
+                break;
+            case 7:
+                //apply the base stats per level
+                HP_max = 140;
+                SP_max = 47;
+                ATK = 36;
+                POW = 36;
+                DEF = 36;
+                WIL = 36;
+                RES = 36;
+                SPD = 36;
+                LCK = 36;
+                break;
+            case 8:
+                //apply the base stats per level
+                HP_max = 159;
+                SP_max = 52;
+                ATK = 41;
+                POW = 41;
+                DEF = 41;
+                WIL = 41;
+                RES = 41;
+                SPD = 41;
+                LCK = 41;
+                break;
+            case 9:
+                //apply the base stats per level
+                HP_max = 178;
+                SP_max = 57;
+                ATK = 46;
+                POW = 46;
+                DEF = 46;
+                WIL = 46;
+                RES = 46;
+                SPD = 46;
+                LCK = 46;
+                break;
+            case 10:
+                //apply the base stats per level
+                HP_max = 197;
+                SP_max = 62;
+                ATK = 51;
+                POW = 51;
+                DEF = 51;
+                WIL = 51;
+                RES = 51;
+                SPD = 51;
+                LCK = 51;
+                break;
+            case 11:
+                //apply the base stats per level
+                HP_max = 216;
+                SP_max = 67;
+                ATK = 56;
+                POW = 56;
+                DEF = 56;
+                WIL = 56;
+                RES = 56;
+                SPD = 56;
+                LCK = 56;
+                break;
+            case 12:
+                //apply the base stats per level
+                HP_max = 235;
+                SP_max = 72;
+                ATK = 61;
+                POW = 61;
+                DEF = 61;
+                WIL = 61;
+                RES = 61;
+                SPD = 61;
+                LCK = 61;
+                break;
+            case 13:
+                //apply the base stats per level
+                HP_max = 254;
+                SP_max = 77;
+                ATK = 66;
+                POW = 66;
+                DEF = 66;
+                WIL = 66;
+                RES = 66;
+                SPD = 66;
+                LCK = 66;
+                break;
+            case 14:
+                //apply the base stats per level
+                HP_max = 173;
+                SP_max = 82;
+                ATK = 71;
+                POW = 71;
+                DEF = 71;
+                WIL = 71;
+                RES = 71;
+                SPD = 71;
+                LCK = 71;
+                break;
+            case 15:
+                //apply the base stats per level
+                HP_max = 292;
+                SP_max = 87;
+                ATK = 76;
+                POW = 76;
+                DEF = 76;
+                WIL = 76;
+                RES = 76;
+                SPD = 76;
+                LCK = 76;
+                break;
+            case 16:
+                //apply the base stats per level
+                HP_max = 311;
+                SP_max = 92;
+                ATK = 81;
+                POW = 81;
+                DEF = 81;
+                WIL = 81;
+                RES = 81;
+                SPD = 81;
+                LCK = 81;
+                break;
+            case 17:
+                //apply the base stats per level
+                HP_max = 330;
+                SP_max = 97;
+                ATK = 86;
+                POW = 86;
+                DEF = 86;
+                WIL = 86;
+                RES = 86;
+                SPD = 86;
+                LCK = 86;
+                break;
+            case 18:
+                //apply the base stats per level
+                HP_max = 349;
+                SP_max = 102;
+                ATK = 91;
+                POW = 91;
+                DEF = 91;
+                WIL = 91;
+                RES = 91;
+                SPD = 91;
+                LCK = 91;
+                break;
+            case 19:
+                //apply the base stats per level
+                HP_max = 368;
+                SP_max = 107;
+                ATK = 96;
+                POW = 96;
+                DEF = 96;
+                WIL = 96;
+                RES = 96;
+                SPD = 96;
+                LCK = 96;
+                break;
+            case 20:
+                //apply the base stats per level
+                HP_max = 387;
+                SP_max = 112;
+                ATK = 101;
+                POW = 101;
+                DEF = 101;
+                WIL = 101;
+                RES = 101;
+                SPD = 101;
+                LCK = 101;
+                break;
+            default:
+                break;
+        }
+
+        //reapply weapon armor and trinket
+        SetWeapon(temp_weapon);
+        SetArmor(temp_armor);
+        SetTrinket(temp_trinket);
     }
 
     public CharacterStats()
