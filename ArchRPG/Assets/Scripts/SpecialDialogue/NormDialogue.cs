f8d549042882ba3dfca4e7da61baae77150c8e6f
James Lillicrap <jamesplillicrap@gmail.com>
2021-02-16 19:02:57 -0500

    +Made a very important change.. .oh and also I cropped the banana

--- ArchRPG/Assets/Scripts/SpecialDialogue/NormDialogue.cs
+++ ArchRPG/Assets/Scripts/SpecialDialogue/NormDialogue.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class NormDialogue : InteractableBaseClass
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class ExpandedString
     {
-        
+        [TextArea(3, 5)]
+        public string text;
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class DialogueImages
     {
-        
+        public string sprite_path;
+        public int lines;
+    }
+
+    [SerializeField]
+    public List<ExpandedString> text;
+    [SerializeField]
+    public List<EffectContainer> container;
+    [SerializeField]
+    public List<DialogueImages> dialogueImages;
+    private List<string> converted_text;
+    private List<string> converted_images;
+    private PlayerDialogueBoxHandler player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDialogueBoxHandler>();
+        converted_text = new List<string>();
+        for (int i = 0; i < text.Count; i++)
+        {
+            converted_text.Add(text[i].text);
+        }
+
+        converted_images = new List<string>();
+        for (int i = 0; i < dialogueImages.Count; i++)
+        {
+            for (int j = 0; j < dialogueImages[i].lines; j++)
+            {
+                converted_images.Add(dialogueImages[i].sprite_path);
+            }
+        }
     }
 
     public override void Interact()
     {
-
+        player.OpenTextBox();
+        player.SetWriteQueue(converted_text);
+        player.SetEffectQueue(container);
+        player.SetImageQueue(converted_images);
+        player.WriteDriver();
     }
 }
