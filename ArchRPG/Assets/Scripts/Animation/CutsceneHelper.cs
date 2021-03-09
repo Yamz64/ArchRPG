3ade779c793be91ceef1d2afea95b13812785ab1
James Lillicrap <jamesplillicrap@gmail.com>
2021-03-09 01:38:48 -0500

    +Added animation ease out and in functions for the Hound sequence

--- ArchRPG/Assets/Scripts/Animation/CutsceneHelper.cs
+++ ArchRPG/Assets/Scripts/Animation/CutsceneHelper.cs
@@ -16,4 +16,42 @@ public static class CutsceneHelper
         }
         character.transform.position = destination;
     }
+
+    public static IEnumerator SlowStartTranslateCharacter(GameObject character, Vector2 destination, float time)
+    {
+        Vector2 start_pos = character.transform.position;
+        float progress = 0;
+        while(progress < time)
+        {
+            progress += 1 / 60f;
+            character.transform.position = Vector3.Lerp(start_pos, destination, (Mathf.Pow(progress, 2)/time) / time);
+            yield return new WaitForSeconds(1f/60f);
+        }
+        character.transform.position = destination;
+    }
+
+    public static IEnumerator EaseOutTranslateCharacter(GameObject character, Vector2 destination, float time)
+    {
+        Vector2 start_pos = character.transform.position;
+        float progress = 0;
+        while(progress < time)
+        {
+            progress += 1f / 60f;
+            character.transform.position = Vector3.Lerp(start_pos, destination, (time * Mathf.Sin((Mathf.PI * progress)/(2*time))) / time);
+            yield return new WaitForSeconds(1 / 60f);
+        }
+    }
+
+    public static IEnumerator InterpColor(SpriteRenderer rend, Color color, float time)
+    {
+        Color start_color = rend.color;
+        float progress = 0;
+        while(progress < time)
+        {
+            progress += 1f / 60f;
+            rend.color = Color.Lerp(start_color, color, progress / time);
+            yield return new WaitForSeconds(1f / 60f);
+        }
+        rend.color = color;
+    }
 }
--- ArchRPG/Assets/Scripts/Animation/SubwaySequence.cs
+++ ArchRPG/Assets/Scripts/Animation/SubwaySequence.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class SubwaySequence : MonoBehaviour
 {
+    IEnumerator TestEase()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            StartCoroutine(CutsceneHelper.EaseOutTranslateCharacter(gameObject, (Vector2)transform.position + Vector2.up * 2, .1f));
+            yield return new WaitForSeconds(2f);
+            transform.position = new Vector2(transform.position.x, transform.position.y - 2);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(TestEase());
     }
 
     // Update is called once per frame
