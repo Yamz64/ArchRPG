1d465f8719c17b4e3b9becd6b3cbcf2c84a1b4e4
James Lillicrap <jamesplillicrap@gmail.com>
2021-03-29 00:42:42 -0400

    +Changes
    +Added factory loading zones
    ~fixed bug where persistent audio would not play between 2 different persistent audio zones

--- ArchRPG/Assets/Scripts/Audio/InstanceGeneralMusic.cs
+++ ArchRPG/Assets/Scripts/Audio/InstanceGeneralMusic.cs
@@ -26,5 +26,19 @@ public class InstanceGeneralMusic : MonoBehaviour
                 instance.GetComponent<GeneralMusicManager>().valid_scenes.Add(valid_scenes[i]);
             }
         }
+        //if there is a music instance in the scene, check to see if it's clip is the correct clip, if it isn't do the above steps
+        else if(GameObject.Find(music_instance.name + "(Clone)").GetComponent<AudioSource>().clip.name != track_name)
+        {
+            GameObject instance = (GameObject)Instantiate(music_instance);
+
+            instance.GetComponent<AudioSource>().clip = Resources.Load<AudioClip>("Sound/Music/" + track_name);
+
+            instance.GetComponent<GeneralMusicManager>().valid_scenes = new List<string>();
+
+            for (int i = 0; i < valid_scenes.Count; i++)
+            {
+                instance.GetComponent<GeneralMusicManager>().valid_scenes.Add(valid_scenes[i]);
+            }
+        }
     }
 }
