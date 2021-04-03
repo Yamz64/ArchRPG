4fa1810505e6865c5e768663d2cc390e8aabce0d
James Lillicrap <jamesplillicrap@gmail.com>
2021-04-03 17:03:29 -0400

    +Added DiscoHooligans dialogue into their combat

--- ArchRPG/Assets/Scripts/SpecialDialogue/DiscoHooligansDialogue.cs
+++ ArchRPG/Assets/Scripts/SpecialDialogue/DiscoHooligansDialogue.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DiscoHooligansDialogue : MonoBehaviour
+public class DiscoHooligansDialogue : NPCDialogue
 {
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator StartFight()
     {
-        
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => player.GetWriteCount() == 0);
+        yield return new WaitUntil(() => !player.GetActive());
+
+        //begin fight with the player
+        CharacterStatJsonConverter data = new CharacterStatJsonConverter(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDataMono>().data);
+        data.SaveEnemyNames("Dylan", "Dan", "Brian");
+        data.active_scene = SceneManager.GetActiveScene().name;
+        data.position = GameObject.FindGameObjectWithTag("Player").transform.position;
+        data.Save(PlayerPrefs.GetInt("_active_save_file_"));
+        GameObject.FindGameObjectWithTag("Player").GetComponent<TransitionHandler>().BattleTransitionDriver();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => GameObject.FindGameObjectWithTag("Player").GetComponent<TransitionHandler>().transition_completed);
+        SceneManager.LoadScene("BattleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    new void Start()
     {
-        
+        base.Start();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        StartCoroutine(StartFight());
     }
 }
