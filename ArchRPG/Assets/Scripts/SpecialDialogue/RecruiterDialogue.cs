c08e2922a55ace9177d4fc6b1ab89e6d5d8c6022
James Lillicrap <jamesplillicrap@gmail.com>
2021-04-10 16:01:57 -0400

    +Finished the recruiter's dialogue

--- ArchRPG/Assets/Scripts/SpecialDialogue/RecruiterDialogue.cs
+++ ArchRPG/Assets/Scripts/SpecialDialogue/RecruiterDialogue.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class RecruiterDialogue : InteractableBaseClass
 {
+    [SerializeField]
+    public Item j_id;
+
+    [SerializeField]
+    public List<string> scenes;
+
     private bool given_lollipop;
     private bool interacted;
+    private bool meatdogs_sold;
+    private bool meatdogs_sold_interacted;
+
 
     private PlayerDialogueBoxHandler player;
     private PauseMenuHandler pause;
@@ -204,10 +213,12 @@ public class RecruiterDialogue : InteractableBaseClass
                 MapDataManager map_data = GameObject.FindGameObjectWithTag("MapManager").GetComponent<MapDataManager>();
                 map_data.SetInteracted("Recruiter", true);
                 map_data.Save();
+                interacted = true;
 
                 PlayerData data = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDataMono>().data;
                 if (!given_lollipop) data.AddItem(new Consumables.BaconLollipop());
                 for (int i = 0; i < 5; i++) { data.AddItem(new Consumables.MeatDog()); }
+                given_lollipop = true;
             }
             //no
             else
@@ -435,10 +446,12 @@ public class RecruiterDialogue : InteractableBaseClass
                     MapDataManager map_data = GameObject.FindGameObjectWithTag("MapManager").GetComponent<MapDataManager>();
                     map_data.SetInteracted("Recruiter", true);
                     map_data.Save();
+                    interacted = true;
 
                     PlayerData data = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDataMono>().data;
                     if (!given_lollipop) data.AddItem(new Consumables.BaconLollipop());
                     for (int i = 0; i < 5; i++) { data.AddItem(new Consumables.MeatDog()); }
+                    given_lollipop = true;
                 }
                 //no
                 else
@@ -462,6 +475,134 @@ public class RecruiterDialogue : InteractableBaseClass
         }
     }
 
+    IEnumerator AltFirstConversationSequence()
+    {
+        yield return new WaitForEndOfFrame();
+
+        //populate dialogue with first conversation
+
+        List<string> text = new List<string>();
+        TextEffectClass effect = new TextEffectClass();
+        EffectContainer container = new EffectContainer();
+        List<EffectContainer> effects = new List<EffectContainer>();
+        List<string> images = new List<string>();
+
+        player.OpenTextBox();
+
+        text.Add("Have you sold al of the meat dogs?");
+        text.Add("No");
+        text.Add("Well get out there! Mr. Goodmeat rewards the hardworking!");
+
+        effect.name = "_NO_EFFECT_";
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        container.effects.Clear();
+        effect.name = "_NO_EFFECT_";
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        container.effects.Clear();
+        effect.name = "_NO_EFFECT_";
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        images.Add("CharacterSprites/BackgroundCharacters/Background Kid");
+        images.Add("CharacterSprites/PC");
+        images.Add("CharacterSprites/BackgroundCharacters/Background Kid");
+
+        player.SetWriteQueue(text);
+        player.SetEffectQueue(effects);
+        player.SetImageQueue(images);
+        player.WriteDriver();
+    }
+
+    IEnumerator SecondConversationSequence()
+    {
+        yield return new WaitForEndOfFrame();
+
+        //populate dialogue with first conversation
+
+        List<string> text = new List<string>();
+        TextEffectClass effect = new TextEffectClass();
+        EffectContainer container = new EffectContainer();
+        List<EffectContainer> effects = new List<EffectContainer>();
+        List<string> images = new List<string>();
+
+        player.OpenTextBox();
+
+        text.Add("I see you've sold all the meat dogs!");
+        text.Add("Don't worry Mr. Goodmeat already knows and has authorized me to give you your payment.");
+        text.Add("You got the Junior Meat Salesman ID!");
+
+        effect.name = "_NO_EFFECT_";
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        container.effects.Clear();
+        effect.name = "_NO_EFFECT_";
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        container.effects.Clear();
+        effect.name = "Color";
+        effect.color = Color.yellow;
+        effect.lower = 9;
+        effect.upper = 29;
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        images.Add("CharacterSprites/BackgroundCharacters/Background Kid");
+        images.Add("CharacterSprites/BackgroundCharacters/Background Kid");
+        images.Add(null);
+
+        player.SetWriteQueue(text);
+        player.SetEffectQueue(effects);
+        player.SetImageQueue(images);
+        player.WriteDriver();
+
+        player.GetComponent<PlayerDataMono>().data.AddItem(new Item(j_id));
+        meatdogs_sold_interacted = true;
+        MapDataManager map_data = GameObject.FindGameObjectWithTag("MapManager").GetComponent<MapDataManager>();
+        map_data.SetInteracted("MeatdogsSold", true);
+        map_data.Save();
+    }
+
+    IEnumerator AltSecondConversationSequence()
+    {
+        yield return new WaitForEndOfFrame();
+
+        //populate dialogue with first conversation
+
+        List<string> text = new List<string>();
+        TextEffectClass effect = new TextEffectClass();
+        EffectContainer container = new EffectContainer();
+        List<EffectContainer> effects = new List<EffectContainer>();
+        List<string> images = new List<string>();
+
+        player.OpenTextBox();
+
+        text.Add("Enjoy your employment to Mr. Goodmeat!");
+        text.Add("Heh heh heh!");
+
+        effect.name = "_NO_EFFECT_";
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        container.effects.Clear();
+        effect.name = "_NO_EFFECT_";
+        container.effects.Add(new TextEffectClass(effect));
+        effects.Add(new EffectContainer(container));
+
+        images.Add("CharacterSprites/BackgroundCharacters/Background Kid");
+        images.Add("CharacterSprites/BackgroundCharacters/Background Kid");
+
+        player.SetWriteQueue(text);
+        player.SetEffectQueue(effects);
+        player.SetImageQueue(images);
+        player.WriteDriver();
+    }
+
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDialogueBoxHandler>();
@@ -472,6 +613,36 @@ public class RecruiterDialogue : InteractableBaseClass
         interacted = map_data.GetInteracted("Recruiter");
         if (interacted) given_lollipop = true;
         else given_lollipop = false;
+
+        //see if the meatdogs have been sold
+        int meatdog_sell_count = 0;
+        MapSaveData s_map_data = new MapSaveData();
+        s_map_data.Load();
+        for(int i=0; i<s_map_data.map_data.Count; i++)
+        {
+            for(int j=0; j<scenes.Count; j++)
+            {
+                if(s_map_data.map_data[i].name == scenes[j])
+                {
+                    for(int k=0; k<s_map_data.map_data[i].objects.Count; k++)
+                    {
+                        if(s_map_data.map_data[i].objects[k].o == "MeatdogEnthusiast" && s_map_data.map_data[i].objects[k].interacted)
+                        {
+                            meatdog_sell_count++;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        if(meatdog_sell_count == 5)
+        {
+            meatdogs_sold = true;
+            //check to see if the meatdog sell interaction has already happened
+            meatdogs_sold_interacted = map_data.GetInteracted("MeatdogsSold");
+        }
     }
 
     public override void Interact()
@@ -479,6 +650,15 @@ public class RecruiterDialogue : InteractableBaseClass
         if (!interacted)
         {
             StartCoroutine(FirstConverstationSequence());
+        }
+        else if (meatdogs_sold)
+        {
+            if (!meatdogs_sold_interacted) StartCoroutine(SecondConversationSequence());
+            else StartCoroutine(AltSecondConversationSequence());
+        }
+        else
+        {
+            StartCoroutine(AltFirstConversationSequence());
         }
     }
 }
