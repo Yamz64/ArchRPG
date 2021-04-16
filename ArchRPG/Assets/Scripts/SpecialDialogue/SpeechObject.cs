8fd6355c23f7b72f0a2907eaf1e0d49ab6f04f33
James Lillicrap <jamesplillicrap@gmail.com>
2021-04-16 13:24:42 -0400

    +Added party member dialogue objects for the speech cutscene

---
+++ ArchRPG/Assets/Scripts/SpecialDialogue/SpeechObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SpeechObject", menuName = "SpeechObject")]
+public class SpeechObject : ScriptableObject
+{
+    public string character_name;
+
+    public List<NPCDialogue.ExpandedString> text;
+    public List<EffectContainer> effects;
+    public List<NPCDialogue.DialogueImages> images;
+
+    [HideInInspector]
+    public List<string> converted_text;
+    [HideInInspector]
+    public List<string> converted_images;
+
+    public SpeechObject()
+    {
+        converted_text = new List<string>();
+        if (text != null)
+        {
+            if (text.Count != 0)
+            {
+                for (int i = 0; i < text.Count; i++)
+                {
+                    converted_text.Add(text[i].text);
+                }
+            }
+        }
+
+        converted_images = new List<string>();
+        if (images != null)
+        {
+            if (images.Count != 0)
+            {
+                for (int i = 0; i < images.Count; i++)
+                {
+                    for (int j = 0; j < images[i].lines; j++)
+                    {
+                        converted_images.Add(images[i].sprite_path);
+                    }
+                }
+            }
+        }
+    }
+}
