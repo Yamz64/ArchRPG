f9df29730a22c0ad3409b0150010c1e219165be1
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2021-04-22 00:14:40 -0400

    Update BattleScript.cs

--- ArchRPG/Assets/Scripts/Battle/BattleScript.cs
+++ ArchRPG/Assets/Scripts/Battle/BattleScript.cs
@@ -3681,6 +3681,16 @@ public class BattleScript : MonoBehaviour
                 else if ((loader.names[i] == "Oliver Sprout" || loader.names[i] == "OliverSprout") && !loader.dead[i])
                 {
                     p = new OliverSproutUnit(loader.levels[i]);
+                    if (i < 2)
+                    {
+                        p.mode = 0;
+                        p.setHUD();
+                    }
+                    else
+                    {
+                        p.mode = 1;
+                        p.setHUD();
+                    }
                 }
                 else if ((loader.names[i] == "Ember Moon" || loader.names[i] == "EmberMoon") && !loader.dead[i])
                 {
