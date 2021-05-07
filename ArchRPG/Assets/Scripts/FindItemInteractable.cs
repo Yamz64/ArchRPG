2c5ee0fbd99bafa5fcc10c411caa61cb8f46fa34
James Lillicrap <jamesplillicrap@gmail.com>
2021-05-07 17:01:30 -0400

    +Item changes
    +Added Occult Text to one of the bookshelves in the library
    +the FindItemInteractable script now can look for specific items

--- ArchRPG/Assets/Scripts/FindItemInteractable.cs
+++ ArchRPG/Assets/Scripts/FindItemInteractable.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class FindItemInteractable : InteractableBaseClass
 {
+    public bool quick = true;
     public bool destroy_in_overworld;
 
     public Item item;
 
     private GameObject player;
 
+    public string id;
     public string get_item_text;
     public string used_text;
+
+    public enum ItemType { Consumable, Weapon, Armor, Trinket };
+    public ItemType type;
 
     public EffectContainer get_item_effect;
     public EffectContainer used_item_effect;
@@ -55,7 +60,27 @@ public class FindItemInteractable : InteractableBaseClass
             dialogue.SetEffectQueue(effect);
             dialogue.WriteDriver();
             used = true;
-            player.GetComponent<PlayerDataMono>().data.AddItem(item);
+            if(quick) player.GetComponent<PlayerDataMono>().data.AddItem(item);
+            else
+            {
+                switch (type)
+                {
+                    case ItemType.Consumable:
+                        player.GetComponent<PlayerDataMono>().data.AddItem(Consumables.InstanceSubclass(id));
+                        break;
+                    case ItemType.Weapon:
+                        player.GetComponent<PlayerDataMono>().data.AddItem(Weapons.InstanceSubclass(id));
+                        break;
+                    case ItemType.Armor:
+                        player.GetComponent<PlayerDataMono>().data.AddItem(Armors.InstanceSubclass(id));
+                        break;
+                    case ItemType.Trinket:
+                        player.GetComponent<PlayerDataMono>().data.AddItem(Trinkets.InstanceSubclass(id));
+                        break;
+                    default:
+                        break;
+                }
+            }
 
             //try to find this object in the map manager, and state it has been interacted with
             MapDataManager map_manager = GameObject.FindGameObjectWithTag("MapManager").GetComponent<MapDataManager>();
