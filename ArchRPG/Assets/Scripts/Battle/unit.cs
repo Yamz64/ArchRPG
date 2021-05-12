d830f5db80dba7fc8970aad45af720b6a04f2948
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2021-05-12 01:36:49 -0400

    Bug Fixing 10
    Inventory should now be updated.
    Aspirating/Vomiting text bug should be fixed.
    Nerfed God HP to 400.
    Still need to work on swapping thing

--- ArchRPG/Assets/Scripts/Battle/unit.cs
+++ ArchRPG/Assets/Scripts/Battle/unit.cs
@@ -9029,7 +9029,7 @@ public class God : unit
         resistances[0] = true;
 
         level = 20;
-        SetHPMax(500);
+        SetHPMax(400);
         expGain = 228061;
         enemy = true;
         capital = 8;
@@ -9063,7 +9063,7 @@ public class God2 : unit
         weaknesses[2] = true;
 
         level = 20;
-        SetHPMax(500);
+        SetHPMax(400);
         expGain = 0;
         enemy = true;
         capital = 8;
